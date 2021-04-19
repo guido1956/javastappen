03ba894faf3f328300517495aea5a88ee0557710
Guido <GuidoDulos@valori.nl>
2021-04-19 14:30:16 +0200

    De eerste 10 stappen

--- src/main/java/Stap010ObjectScanner.java
+++ src/main/java/Stap010ObjectScanner.java
@@ -1,6 +1,16 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Stap010ObjectScanner {
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Voer een dier in");
+        String invoer = scanner.nextLine();
+        System.out.println(invoer.toUpperCase());
+    }
+
+
 
 }
